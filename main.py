0614e4d985c114aefd682938e55f647541b7b498
miragest <jiscales.94@gmail.com>
2020-04-23 22:11:02 -0400

    Narrowed Div

--- main.py
+++ main.py
@@ -3,4 +3,8 @@ import requests, bs4, lxml
 url = "https://parahumans.wordpress.com/table-of-contents/"
 page = requests.get(url)
 doc = bs4.BeautifulSoup(page.content, 'html')
-print(doc)
+divc = doc.find('div', attrs = {'class': 'entry-content'})
+
+for link in divc.find_all('a'): 
+    print(link.get('href')) 
+
