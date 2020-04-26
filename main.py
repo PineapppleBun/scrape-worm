c8e0b1b5cf7e3262620fe22295b126068758adc1
PineapppleBun <paulvulf@gmail.com>
2020-04-26 16:36:02 -0400

    Cleaned up page grab

--- main.py
+++ main.py
@@ -2,15 +2,18 @@ import requests, bs4, lxml, time
 
 #Function to seperate content div and extract and print text for individual page
 
-def get_text(link):
-    page = requests.get(link)
-    doc = bs4.BeautifulSoup(page.content, 'lxml')
-    divc = doc.find('div', attrs = {'class': 'entry-content'})
-    for p in divc.find_all('p'):
-        print(p.get_text(),"\n")
+def get_page(link):
+    request = requests.get(link)
+    page_html = bs4.BeautifulSoup(request.content, 'lxml')
+    page_header = page_html.find('h1', attrs = {'class': 'entry-title'})
+    print(page_header.get_text(),"\n")
+    page_content = page_html.find('div', attrs = {'class': 'entry-content'})
+    for paragraph in page_content.find_all('p'):
+        if paragraph.a == None:
+            print(paragraph.get_text(),"\n")
 
 url = "https://parahumans.wordpress.com/category/stories-arcs-1-10/arc-1-gestation/1-01/"
 
 links = []
 
-get_text(url)
+get_page(url)
