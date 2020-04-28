8ed5b9a74aa31cfc20c2b23a7d36581e7aad5251
PineapppleBun <paulvulf@gmail.com>
2020-04-28 19:36:51 -0400

    Changes

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def get_page(link):
     request = requests.get(link)
     page_html = bs4.BeautifulSoup(request.content, 'lxml')
     page_header = page_html.find('h1', attrs = {'class': 'entry-title'})
-    print(page_header.get_text(),"\n")
+    print("-----------------NEXT CHAPTER-----------------\n",page_header.get_text(),"\n")
     page_content = page_html.find('div', attrs = {'class': 'entry-content'})
     for paragraph in page_content.find_all('p'):
         if paragraph.a == None:
@@ -18,12 +18,12 @@ def get_page(link):
 def get_link(page_html):
     page_link = page_html.find('a', text = "Next Chapter")
     if page_link is None:
-      quit()
+        pass
     else:
       url = page_link.get('href')
       return url
 
 #Loop through pages
-while url != 'None':
+while url != None:
     page_html = get_page(url)
     url = get_link(page_html)
