8c1d4ae5497822f0ccde0dea5b71e343bcde124e
miragest <jiscales.94@gmail.com>
2020-04-28 13:36:08 -0400

    Changed a tag criteria

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests, bs4, lxml, time
 
 url = "https://parahumans.wordpress.com/category/stories-arcs-1-10/arc-1-gestation/1-01/"
 
-#Function to extract page content and grab link for next chapter
+#Function to extract page text
 def get_page(link):
     request = requests.get(link)
     page_html = bs4.BeautifulSoup(request.content, 'lxml')
@@ -12,10 +12,18 @@ def get_page(link):
     for paragraph in page_content.find_all('p'):
         if paragraph.a == None:
             print(paragraph.get_text(),"\n")
-    page_link = page_html.find('a', attrs = {'title': 'Next Chapter'})
-    url = page_link.get('href')
-    print(url)
+    return page_html
 
-for i in range(0,2):
-	get_page(url)
+#Function to get page link
+def get_link(page_html):
+    page_link = page_html.find('a', text = "Next Chapter")
+    if page_link is None:
+      quit()
+    else:
+      url = page_link.get('href')
+      return url
 
+#Loop through pages
+while url != 'None':
+    page_html = get_page(url)
+    url = get_link(page_html)
