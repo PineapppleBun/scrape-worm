243d6f347d2a1bbbf1b7b9f80355540963cb6985
PineapppleBun <paulvulf@gmail.com>
2020-04-28 20:34:51 -0400

    Completed writing to files

--- main.py
+++ main.py
@@ -7,11 +7,14 @@ def get_page(link):
     request = requests.get(link)
     page_html = bs4.BeautifulSoup(request.content, 'lxml')
     page_header = page_html.find('h1', attrs = {'class': 'entry-title'})
-    print(page_header.get_text(),"\n")
+    print(page_header.get_text(),"\n\n")
     page_content = page_html.find('div', attrs = {'class': 'entry-content'})
-    for paragraph in page_content.find_all('p'):
-        if paragraph.a == None:
-            print(paragraph.get_text(),"\n")
+    with open(str(page_header.get_text())+".txt", 'a') as chapter:
+        chapter.write(page_header.get_text()+"\n\n")
+        for paragraph in page_content.find_all('p'):
+            if paragraph.a == None:
+                chapter.write(paragraph.get_text()+"\n\n")
+            #print(paragraph.get_text(),"\n")
     return page_html
 
 #Function to get page link
